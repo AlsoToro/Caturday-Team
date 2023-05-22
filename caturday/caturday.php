8a4f70ed13fddd833e4269d96f82bdbce04d9df5
AlsoToro <133999618+AlsoToro@users.noreply.github.com>
2023-05-22 18:15:42 +0300

    Add files via upload

---
+++ caturday/caturday.php
@@ -0,0 +1,8 @@
+<?php 
+require '../templates/main-header.php';
+require '../templates/footer.php';
+require '../templates/main-hero.php';
+require '../templates/content-home.php';
+require '../templates/sidebar.php';
+?>
+      
